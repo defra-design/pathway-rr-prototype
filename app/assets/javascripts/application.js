3f613ff1a6d79e5d4e405ea2af39911c80116719
Tom <thomas.brown1@digital.hmrc.gov.uk>
2022-05-23 09:40:18 +0100

    Added includes/pathway_stat_cards.html to update dynamic cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,4 +8,28 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
   window.MOJFrontend.initAll()
+  
+   
+  var calculateCountry = req.session.data["country"];
+	var calculateHost = req.session.data["host"];
+	var calculateFormat = req.session.data["format"];
+	// var calculateFormatFixed = req.session.data['formatFixed']
+
+	var numPestStat = "";
+	var unmitigateStat = "";
+	var mitigateStat = "";
+
+	// Calculate some stuff
+	if (calculateCountry == "Egypt" && calculateHost == "Solanum tuberosum" && calculateFormat == "Bulbs or tubers") {
+		// Send user to next page
+	}
+  $(".numPestStat").html(numPestStat);
+  $(".unmitigateStat").html(unmitigateStat);
+  $(".mitigateStat").html(mitigateStat);
+
+  console.log(req.session.data);
+  
 })
+
+
+
--- app/v1-1.js
+++ app/v1-1.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // var folder = "current"
 
@@ -9,56 +9,37 @@ const router = express.Router()
 // });
 
 // Run this code when a form is submitted to 'user-type'
-router.post('/user-type', function (req, res) {
-
-  // Make a variable and give it the value from 'user-type'
-  var userType = req.session.data['user-type']
-  // Check whether theuserTypetchesPolicyion
-  if (userType == "policy"){
-    // Send user to next page
-    res.redirect('policy-search')
-  } else {
-    // Send user to trade page
-    res.redirect('trade-search')
-  }
-
-})
+router.post("/user-type", function (req, res) {
+	// Make a variable and give it the value from 'user-type'
+	var userType = req.session.data["user-type"];
+	// Check whether theuserTypetchesPolicyion
+	if (userType == "policy") {
+		// Send user to next page
+		res.redirect("policy-search");
+	} else {
+		// Send user to trade page
+		res.redirect("trade-search");
+	}
+});
 
 // Run this code when a form is submitted to 'policy-search'
-router.post('/policy-calculate', function (req, res) {
+router.post("/policy-calculate", function (req, res) {
+	// Make a variable and give it the value from 'policy-search'
+	var calculateCountry = req.session.data["country"];
+	var calculateHost = req.session.data["host"];
+	var calculateFormat = req.session.data["format"];
+	// var calculateFormatFixed = req.session.data['formatFixed']
 
-  // Make a variable and give it the value from 'policy-search'
-  var calculateCountry = req.session.data['country']
-  var calculateHost = req.session.data['host']
-  var calculateFormat = req.session.data['format']
-  // var calculateFormatFixed = req.session.data['formatFixed']
+	var numPestStat;
+	var unmitigateStat;
+	var mitigateStat;
 
-  // Calculate some stuff
-  if (calculateCountry && calculateHost && calculateFormat  != " " ){
-    // Send user to next page
-    res.redirect('policy-results')
-  }
+	// Calculate some stuff
+	if (calculateCountry && calculateHost && calculateFormat != " ") {
+   
+		// Send user to next page
+		res.redirect("policy-results");
+	}
+});
 
-})
-
-// Run this code when a form is submitted to 'policy-search'
-router.post('/policy-calculate-fixed', function (req, res) {
-
-  // Make a variable and give it the value from 'policy-search'
-  var calculateCountry = req.session.data['country']
-  var calculateHost = req.session.data['host']
-  var calculateFormat = req.session.data['format']
-  // var calculateFormatFixed = req.session.data['formatFixed']
-
-  // Calculate some stuff
-  if (calculateCountry && calculateHost && calculateFormat  != " " ){
-    // Send user to next page
-    res.redirect('policy-results')
-  }
-
-})
-
-
-
-
-module.exports = router
+module.exports = router;
