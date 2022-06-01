55161fbeaccd8433a0dbe27d53c33cada34ee39a
Tom <thomas.brown1@digital.hmrc.gov.uk>
2022-06-01 13:03:35 +0100

    Added  app/assets/images/argentina.png         app/assets/images/norway.png         app/views/v1-3/policy-search-error.html

--- app/v1-3.js
+++ app/v1-3.js
@@ -1,45 +1,36 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-// var folder = "current"
-
-// Routes files for URLs/folders
-// router.get('/user-type', function(req, res) {
-//   res.render(folder + '/user-type');
-// });
 
 // Run this code when a form is submitted to 'user-type'
 router.post("/user-type", function (req, res) {
 	// Make a variable and give it the value from 'user-type'
-	var userType = req.session.data["user-type"];
+	var userType = req.session.data["user-type"]
 	// Check whether theuserTypetchesPolicyion
 	if (userType == "policy") {
 		// Send user to next page
-		res.redirect("policy-search");
+		res.redirect("policy-search")
 	} else {
 		// Send user to trade page
-		res.redirect("trade-search");
+		res.redirect("trade-search")
 	}
-});
+})
 
 // Run this code when a form is submitted to 'policy-search'
 router.post("/policy-calculate", function (req, res) {
 	// Make a variable and give it the value from 'policy-search'
-	var calculateCountry = req.session.data["country"];
-	var calculateHost = req.session.data["host"];
-	var calculateFormat = req.session.data["format"];
-	// var calculateFormatFixed = req.session.data['formatFixed']
-
-	var numPestStat;
-	var unmitigateStat;
-	var mitigateStat;
+	var calculateCountry = req.session.data["country"]
+	var calculateHost = req.session.data["host"]
+	var calculateFormat = req.session.data["format"]
 
 	// Calculate some stuff
-	if (calculateCountry && calculateHost && calculateFormat != " ") {
-   
+
+	if (calculateCountry  && calculateHost && calculateFormat ) {
 		// Send user to next page
-		res.redirect("policy-results");
+		res.redirect("policy-results")
+	} else {
+		res.redirect("policy-search-error")
 	}
-});
+})
 
 module.exports = router;
